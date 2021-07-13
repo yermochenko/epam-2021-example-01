6f63336151a8f1193c8ac3a60e7205005e61b5cf
Sergey Yermochenko <yermochenko@gmail.com>
2021-07-13 09:24:50 +0300

    Созданы immutable классы-справочники

--- src/main/java/com/epam/domain/FoodType.java
+++ src/main/java/com/epam/domain/FoodType.java
@@ -4,19 +4,16 @@ public class FoodType {
     private Long id;
     private String name;
 
+    public FoodType(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
--- src/main/java/com/epam/domain/TransportType.java
+++ src/main/java/com/epam/domain/TransportType.java
@@ -4,19 +4,16 @@ public class TransportType {
     private Long id;
     private String name;
 
+    public TransportType(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
